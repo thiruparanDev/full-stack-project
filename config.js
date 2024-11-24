fbc546dd583bdd025d66acc806ae55f066fea1b8
thiruparanb@gmail.com <thiruparanb@gmail.com>
2024-11-24 20:01:26 +1100

    project initialition

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    // mongoUri: process.env.MONGO_URI, 
+    mongoUri: MONGO_URI = 'mongodb+srv://thiruparab:singaporenew@cluster0.taoo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+    PORT: process.env.PORT || 3000,
+}
